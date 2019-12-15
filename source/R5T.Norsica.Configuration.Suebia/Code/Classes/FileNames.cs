4aa7bad5de37f87077fca0d12c89265a9a692028
MinexAutomation <minex.automation@gmail.com>
2019-12-14 23:26:49 -0800

    Added FileNames and configuration-builder extension to add NuGet configuration JSON file.

---
+++ source/R5T.Norsica.Configuration.Suebia/Code/Classes/FileNames.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Norsica.Configuration.Suebia
+{
+    public static class FileNames
+    {
+        public const string NuGetConfigurationJsonFileName = "Configuration-NuGet.json";
+    }
+}
---
+++ source/R5T.Norsica.Configuration.Suebia/Code/Extensions/IConfigurationBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+
+using R5T.Suebia.Extensions;
+
+
+namespace R5T.Norsica.Configuration.Suebia
+{
+    public static class IConfigurationBuilderExtensions
+    {
+        public static IConfigurationBuilder AddNuGetConfigurationJsonFile(this IConfigurationBuilder configurationBuilder, IServiceProvider configurationServiceProvider)
+        {
+            configurationBuilder.AddJsonSecretsFilePath(configurationServiceProvider, FileNames.NuGetConfigurationJsonFileName);
+
+            return configurationBuilder;
+        }
+    }
+}
