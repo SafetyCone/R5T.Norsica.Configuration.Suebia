487c66f3902d615d9fe614a881146de0a452eef5
MinexAutomation <minex.automation@gmail.com>
2019-12-14 23:15:15 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Norsica.Configuration.Suebia.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace R5T.Norsica.Configuration.Suebia.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ source/R5T.Norsica.Configuration.Suebia/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Norsica.Configuration.Suebia
+{
+    public class Class1
+    {
+    }
+}
